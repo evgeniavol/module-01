89fbd1dadc5f037dd34dffc9823d1cce7367066a
Evgenia <evgeniavolkova120@gmail.com>
2021-12-20 19:26:28 +0300

    lesson-13

--- lesson-1-13/game.js
+++ lesson-1-13/game.js
@@ -1,21 +1,62 @@
 "use strict";
 
 (() => {
-const FIGURES_ENG = ['rock', 'scissors', 'paper'];
-const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
+    const FIGURES_ENG = ['rock', 'scissors', 'paper'];
+    const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
 
-const getRandomIntInclusive = (min, max) => {
+    const getRandomIntInclusive = (min, max) => {
 
-};
+    };
 
-const getFigure = lang => {
+    const getFigure = lang => {
 
-};
+    };
 
-const game = () => {
 
-};
+    const game = () => {
 
-window.RPS = game;
+        let playerChoice = prompt('камень, ножницы, бумага?');
+        let userPoints = 0;
+        let computerPoints = 0;
+
+        if (playerChoice) {
+            let playerOne = playerChoice.trim().toLowerCase();
+            if (playerOne === 'камень' ||
+                playerOne === 'ножницы' ||
+                playerOne === 'бумага'
+            ) {
+                let computerChoice = Math.floor(Math.random() * 3 + 1);
+                let computer = computerChoice === 1 ? 'камень' : computerChoice === 2 ? 'бумага' : 'ножницы';
+                if (playerOne === computer) {
+                    alert('Ничья');
+                    userPoints++;
+                    computerPoints++;
+                    alert('Ещё?');
+                } else if (playerOne === 'камень' && computer === 'бумага') {
+                    alert(`Игрок: ${playerOne}\nКомпьютер: ${computer}
+            \nВыиграл компьютер!`);
+                    computerPoints++;
+                    alert('Ещё?');
+                } else if (playerOne === 'бумага' && computer === 'ножницы') {
+                    alert(`Игрок: ${playerOne}\nКомпьютер: ${computer}
+            \nВыиграл компьютер!`);
+                    computerPoints++;
+                    alert('Ещё?');
+
+                } else if (playerOne === 'ножницы' && computer === 'камень') {
+                    alert(`Игрок: ${playerOne}\nКомпьютер: ${computer}\nВы выиграли!`);
+                    userPoints++;
+                    alert('Ещё?');
+                }
+            } else {
+                alert("Вы не ввели камень, ножницы, бумага.");
+            }
+            game(playerChoice);
+        } else {
+            alert('Точно ли хочешь покинуть игру?');
+            alert(`Результат:\nИгрок набрал: ${userPoints} очк.\nКомпьютер набрал: ${computerPoints} очк.`);
+        }
+    };
+    window.RPS = game;
 
 })();
--- lesson-1-13/script.js
+++ lesson-1-13/script.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 
-// const startGame = window.RPS();
-// startGame();
+const startGame = window.RPS();
+startGame();
