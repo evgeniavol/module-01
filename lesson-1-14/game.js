24ef11b973018787b230380396efef2ce99f591b
Evgenia <evgeniavolkova120@gmail.com>
2021-12-27 20:41:52 +0300

    lesson-14

--- lesson-1-14/game.js
+++ lesson-1-14/game.js
@@ -1,30 +1,97 @@
 "use strict";
 
-let playerBalls = 5;
-let computerBalls = 5;
+let compCountBalls = 5;
+let playerCountBalls = 5;
+
+let guessCompBalls;
 let compEvenOdd;
+let numBot;
+
+
+function randomCompBall(max) {
+    return Math.round(Math.random() * (max - 1) + 1)
+}
+
+function compGuess() {
+    guessCompBalls = randomCompBall(compCountBalls);
+    compEvenOdd = Math.round((Math.random() * 1));
+}
+
+function checkEven(num) {
+    return (num % 2 === 0) ? 'четное' : 'нечетное';
+}
 
 function playGame() {
-
     function play() {
+        compGuess();
+        let playerStep = parseInt(prompt('Сколько шариков загадываете ?'));
+        if (playerStep % 2 === 0 && compEvenOdd === 0) {
+            compCountBalls += playerStep;
+            playerCountBalls -= playerStep;
 
-        let playerStep = parseInt(prompt(`Загадай число от 1 до ${playerBalls}`));
-        console.log(playerStep);
+            if (playerCountBalls < 0) {
+                alert('Игра окончена игрок проиграл');
+                return;
+            }
+            if (confirm(`Бот выбрал ${checkEven(compEvenOdd)}:
+                бот: ${compCountBalls};
+                игрок: ${playerCountBalls};`) === false) {
+                return;
+            };
+            alert('Ход бота');
 
-        let compEvenOdd = Math.floor((Math.random() * 1) + 0.5);
-        console.log(compEvenOdd);
+            numBot = guessCompBalls;
 
-        if (playerStep % 2 === 0 && compEvenOdd % 2 === 0) {
-            computerBalls += playerStep;
-            playerBalls -= playerStep;
+            let answerPlayer = prompt('Введите четное или нечетное (ч или н) ?');
+
+            if (checkEven(numBot) === 'четное' && answerPlayer === 'ч') {
+                compCountBalls -= numBot;
+                playerCountBalls += numBot;
+
+                if (compCountBalls < 0) {
+                    alert('Игра окончена бот проиграл');
+                    return;
+                }
+
+                confirm(`Бот загадал ${numBot}:
+                    бот: ${compCountBalls};
+                    игрок: ${playerCountBalls};`);
+                alert('Ход игрока');
+
+            } else {
+                compCountBalls += numBot;
+                playerCountBalls -= numBot;
+
+                if (playerCountBalls < 0) {
+                    alert('Игра окончена игрок проиграл');
+                    return;
+                }
+
+                confirm(`Бот загадал ${numBot}:
+                    бот: ${compCountBalls};
+                    игрок: ${playerCountBalls};`);
+                alert('Ход бота');
+                play();
+
+            }
         } else {
-            playerBalls += playerStep;
-            computerBalls -= playerStep;
+            compCountBalls -= playerStep;
+            playerCountBalls += playerStep;
+
+            if (compCountBalls < 0) {
+                alert('Игра окончена бот проиграл');
+                return;
+            }
+
+            if (confirm(`Бот выбрал ${compEvenOdd}:
+            бот: ${compCountBalls};
+            игрок: ${playerCountBalls};`)) {
+                alert
+            };
+            alert('Ход игрока');
+            play();
         }
     }
-    console.log(computerBalls);
-    console.log(playerBalls);
     play();
-};
-
+}
 playGame();
